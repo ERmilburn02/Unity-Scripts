6a70317d3978924a76cc16503e21478374bbb14f
ERmilburn02 <61363431+ERmilburn02@users.noreply.github.com>
2021-03-02 11:05:49 +0000

    Create HiResScreenShots.cs

---
+++ graphics/HiResScreenShots.cs
@@ -0,0 +1,52 @@
+// Code by https://answers.unity.com/users/96/jashan.html
+
+// This code relies on the old Input System, and takes a 1080p screenshot from the main camera's viewport, no matter the resolution the game is running at.
+using UnityEngine;
+
+public class HiResScreenShots : MonoBehaviour
+{
+    public int resWidth = 1920;
+    public int resHeight = 1080;
+
+    private bool takeHiResShot = false;
+    private Camera cam;
+
+    public static string ScreenShotName(int width, int height)
+    {
+        return string.Format("{0}/Screenshots/screen_{1}x{2}_{3}.png",
+                             System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyPictures),
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void TakeHiResShot()
+    {
+        takeHiResShot = true;
+    }
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        takeHiResShot |= Input.GetKeyDown("k");
+        if (takeHiResShot)
+        {
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            cam.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            cam.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            cam.targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = ScreenShotName(resWidth, resHeight);
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log(string.Format("Took screenshot to: {0}", filename));
+            takeHiResShot = false;
+        }
+ 
